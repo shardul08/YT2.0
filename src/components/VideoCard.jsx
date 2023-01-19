94e801110ff4f35d5abb0347d38a02741f3b5cb1
Shardul Silswal <sshardul007@gmail.com>
2023-01-19 18:57:47 +0530

    Show suggested videos

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,12 +6,12 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelTitle, demoC
 
 const VideoCard = ({ video: { id: { videoId}, snippet} }) => {
     return (
-        <Card sx={{ width: { md: '320px', xs: '100%'}, boxShadow: 'none', borderRadius: '0' }}>
+        <Card sx={{ width: { xs: '100%', sm: '358px', md: '320px' }, boxShadow: 'none', borderRadius: '0' }}>
             <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
                 <CardMedia 
                     image={snippet?.thumbnails?.high?.url}
                     alt={snippet?.tite}
-                    sx={{ width: 358, height: 180}}
+                    sx={{ width: { xs: '100%', sm: '358px', md: '320px' }, height: 180}}
                 />
             </Link>
             <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px'}}>
--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -9,22 +9,26 @@ import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
     const [videoDetail, setVideoDetail] = useState(null);
+    const [videos, setVideos] = useState([]);
     const { id } = useParams();
 
     useEffect(() => {
         fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
             .then((data) => {setVideoDetail(data.items[0])});
+        
+            fetchFromAPI(`search?relatedToVideoId=${id}&part=snippet&type=video`)
+            .then((data) => {setVideos(data.items)});
     }, [id])
 
     if(!videoDetail?.snippet)
         return 'Loading...';    
     const { snippet : { title, channelId, channelTitle}, statistics: { viewCount, likeCount} } = videoDetail;
-
+    console.log(videos);
     return (
         <Box minHeight='95vh'>
             <Stack direction={{ xs: 'column', md:'row' }}>
                 <Box flex={1}>
-                    <Box sx={{ position: 'sticky', width: '100%'}}>
+                    <Box sx={{ position: 'sticky', width: '100%', top: '86px' }}>
                         <ReactPlayer 
                             url={`https://www.youtube.com/watch?v=${id}`}
                             className='react-player'
@@ -50,6 +54,9 @@ const VideoDetail = () => {
                             </Stack>
                         </Stack>
                     </Box>
+                </Box>
+                <Box px={2} py={{ md: 1, xs: 5}} justifyContent='center' alignItems={'center'}>
+                    <Videos videos={videos} direction='column'/>
                 </Box>
             </Stack>
         </Box>
